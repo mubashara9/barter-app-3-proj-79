6f57a58d607c3427e38aa089c85a4d2553b8215a
mubashara9 <63541309+mubashara9@users.noreply.github.com>
2021-04-05 16:23:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {AppTabNavigator} from './components/AppTabNavigator';
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <AppContainer/>
+  );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+const switchNavigator= createSwitchNavigator({
+  WelcomeScreen:{screen:WelcomeScreen},
+  BottomTab:{screen:AppTabNavigator}
+})
+
+const AppContainer = createAppContainer(switchNavigator);
